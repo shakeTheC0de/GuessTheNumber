09f72ce50adb7604a0eb27fc073e5e4e75ad880a
unknown <shakethecode@gmail.com>
2023-08-16 14:03:40 +0200

    Primer commit

---
+++ src/main.rs
@@ -0,0 +1,40 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Guess the number!!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    
+
+    loop{
+
+        println!("Please type a number!");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+
+    println!("The secret number is {secret_number}");
+}
